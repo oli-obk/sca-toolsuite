86e60fb99e637925da3688356aeb3ab9f310ba73
Johannes Lorenz <johannes89@mailueberfall.de>
2012-06-06 14:33:49 +0200

    Added forgotten files.

---
+++ src/res/rotor_algorithm.h
@@ -0,0 +1,110 @@
+/*************************************************************************/
+/* sca toolsuite - a toolsuite to simulate sandpile cellular automata.   */
+/* Copyright (C) 2011-2012                                               */
+/* Johannes Lorenz                                                       */
+/* https://github.com/JohannesLorenz/sca-toolsuite                       */
+/*                                                                       */
+/* This program is free software; you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation; either version 3 of the License, or (at */
+/* your option) any later version.                                       */
+/* This program is distributed in the hope that it will be useful, but   */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      */
+/* General Public License for more details.                              */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with this program; if not, write to the Free Software           */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA  */
+/*************************************************************************/
+
+#ifndef ROTOR_ALGORITHM_H
+#define ROTOR_ALGORITHM_H
+
+#include <stack_algorithm.h>
+
+const int INVERT_BIT = (1 << 31);
+
+template<class AvalancheContainer>
+inline void inc_push_neighbour(std::vector<int>* chips, int position, AvalancheContainer* array)
+{
+	++(*chips)[position];
+	if((*chips)[position]>=0)
+	{
+		(*chips)[position] |= INVERT_BIT;
+		array->push(position);
+	}
+}
+
+template<class AvalancheContainer, class ResultType>
+inline void rotor_fix(std::vector<int>* grid, std::vector<int>* chips,
+	const dimension* dim, int hint, AvalancheContainer* array, ResultType* result_logger)
+{
+	const int INVERT_BIT = (1 << 31);
+	const int GRAIN_BITS = (-1) ^ INVERT_BIT;
+
+	const int PALETTE[7] = { -dim->width, 1, dim->width, -1, -dim->width, 1, dim->width };
+
+	if((*chips)[hint]>0) // otherwise, we would need an additional "case 0" label
+	{
+		array->push(hint);
+		result_logger->write_avalanche_counter();
+
+		do {
+			const int cur_element = array->pop();
+			//result_logger->write_int_to_file(&cur_element); // TODO
+
+			(*chips)[cur_element] &= GRAIN_BITS;
+			int cur_rotor = (*grid)[cur_element];
+			int chips_to_dec = std::min(3,(*chips)[cur_element]);
+
+			//printf("%d chips, rotor: %d\n", (*chips)[cur_element], (*grid)[cur_element]);
+			switch(chips_to_dec) // assumption: more will no hapen
+			{
+				// unrolled for loop
+				case 3: ++cur_rotor; inc_push_neighbour(chips, cur_element + PALETTE[cur_rotor], array);
+				case 2: ++cur_rotor; inc_push_neighbour(chips, cur_element + PALETTE[cur_rotor], array);
+				case 1: ++cur_rotor; inc_push_neighbour(chips, cur_element + PALETTE[cur_rotor], array); break;
+			/*	default: {
+					std::string err_str = "Invalid number of chips detected."; // TODO: number
+					throw(err_str);
+				}*/
+			}
+			(*grid)[cur_element] = ((*grid)[cur_element] + chips_to_dec)%4;
+			if( ((*chips)[cur_element] -= chips_to_dec) )
+			 array->push(cur_element);
+		} while( ! array->empty() );
+	}
+	result_logger->write_separator();
+	array->flush(); // note: empty does not always imply being flushed!
+}
+
+template<class AvalancheContainer, class ResultType>
+inline void rotor_fix_naive(std::vector<int>* grid, std::vector<int>* chips,
+	const dimension* dim, AvalancheContainer* array, ResultType* result_logger)
+{
+	const int PALETTE[7] = { -dim->width, 1, dim->width, -1, -dim->width, 1, dim->width };
+	//unsigned int area = dim->area();
+	int cur_field;
+	for(unsigned int y = 1; y<(dim->height-1); y++)
+	for(unsigned int x = 1; x<(dim->width-1); x++)
+	{
+		unsigned int coord = (y*dim->width)+x;
+		//if(is_border(dim, (y*dim->width)+x))
+		//	exit(99);
+		for(int nchips = (*chips)[coord]; nchips; --nchips)
+		{
+			cur_field = coord;
+			do
+			{
+			//	printf("cur field: %d\n",cur_field);
+
+				cur_field += PALETTE[(++(*grid)[cur_field])%=4];
+			}
+			while((*grid)[cur_field]>=0); // = not border
+		}
+	}
+	result_logger->write_separator();
+}
+
+#endif // ROTOR_ALGORITHM_H
---
+++ src/rotor/rotor.cpp
@@ -0,0 +1,104 @@
+/*************************************************************************/
+/* sca toolsuite - a toolsuite to simulate sandpile cellular automata.   */
+/* Copyright (C) 2011-2012                                               */
+/* Johannes Lorenz                                                       */
+/* https://github.com/JohannesLorenz/sca-toolsuite                       */
+/*                                                                       */
+/* This program is free software; you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation; either version 3 of the License, or (at */
+/* your option) any later version.                                       */
+/* This program is distributed in the hope that it will be useful, but   */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      */
+/* General Public License for more details.                              */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with this program; if not, write to the Free Software           */
+/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA  */
+/*************************************************************************/
+
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include "io.h"
+#include "rotor_algorithm.h"
+
+template<class AvalancheContainer, class Logger>
+void run(std::vector<int>& grid, std::vector<int>& chips, const dimension& dim, int hint=-1)
+{
+	AvalancheContainer container(dim.area_without_border());
+	Logger logger(stdout);
+	if(hint == -1)
+	{
+		/*for(unsigned int i=0; i<dim.area_without_border(); i++)
+		{
+			const int internal = human2internal(i, dim.width);
+			rotor_fix(&grid, &chips, &dim, internal, &container, &logger);
+		}*/
+		//fix(&chips, &dim, &container, &logger);
+		rotor_fix_naive(&grid, &chips, &dim, &container, &logger);
+	}
+	else
+	 rotor_fix(&grid, &chips, &dim, human2internal(hint, dim.width), &container, &logger);
+}
+
+class MyProgram : public Program
+{
+	int main()
+	{
+		FILE* read_fp = stdin;
+		int hint = -1;
+		char output_type = 's';
+		const char* shell_command = NULL;
+
+		switch(argc) {
+			case 4: hint = atoi(argv[3]);
+			case 3: shell_command = argv[2];
+				output_type = argv[1][0];
+				if(argv[1][1] || !(output_type=='l'||output_type=='s'))
+				 exit_usage();
+				break;
+			default:
+				exit_usage();
+				return 1;
+		}
+
+		std::vector<int> grid;
+		dimension dim;
+
+		read_grid(read_fp, &grid, &dim);
+
+		std::vector<int> chips;
+		dimension dim2;
+		get_input(argv[2]);
+		read_grid(read_fp, &chips, &dim2);
+
+		if(dim != dim2)
+		 exit("Different dimensions in both grids are not allowed.");
+
+		switch(output_type) {
+			case 'l': ::run<ArrayStack, FixLogL>(grid, chips, dim, hint); break;
+			case 's':
+				::run<ArrayStack, FixLogS>(grid, chips, dim, hint);
+				write_grid(stdout, &grid, &dim);
+				break;
+		}
+
+		return 0;
+	}
+};
+
+int main(int argc, char** argv)
+{
+	HelpStruct help;
+	help.description = "Runs the rotor router algorithm until all chips are out.";
+	help.input = "arrow grid";
+	help.syntax = "rotor/rotor s|l <shell command> [<hint>]";
+	help.add_param("s|l", "s calculates resulting arrows, l the number each arrow fires");
+	help.add_param("<shell command>", "calculates chip configuration to add");
+	help.add_param("<hint>", "only ensures that arrow at hint will be fired");
+
+	MyProgram program;
+	return program.run(argc, argv, &help);
+}
